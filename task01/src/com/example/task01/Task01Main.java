dd613e8a3f15adcde15dedc34d9c02b0e5eb5c6d
leclud <negativchikxxl@gmail.com>
2025-03-14 16:52:00 +0500

    1 task

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -3,5 +3,18 @@ package com.example.task01;
 public class Task01Main {
     public static void main(String[] args) {
 
+        Logger logger = Logger.getLogger("FirstLogger");
+
+        Logger sameLogger = Logger.getLogger("FirstLogger");
+        System.out.println("Проверка на одинаковость логгеров: " + (logger == sameLogger));
+        System.out.println("____________");// тру
+
+        logger.setLevel(Logger.LogLevel.DEBUG);
+        System.out.println("Уровень логирования: DEBUG");
+        logger.info("Информационное сообщение"); // Видно
+        logger.debug("Сообщение должно быть видно"); // Видно
+        logger.warning("Сообщение об предупреждении: %s%n", "пум пум пум"); // Видно
+        logger.error("Сообщение об ошибке"); // Видно
+        System.out.println("____________");
     }
 }
